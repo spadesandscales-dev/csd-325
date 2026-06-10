ada01ce3d38f1a7673d9c6f7a4a71e1e5a16a1c7
Austin <spadesandscales@gmail.com>
2026-06-09 21:37:37 -0500

    Pushing updated assignment 1.3 6/9/2026

--- module-1/Lineberry_1.3.py
+++ module-1/Lineberry_1.3.py
@@ -5,18 +5,26 @@
 
 #This is some code to take input and recite the "beer on the wall" song
 
+#Get number of bottles from user, save to bottles variable
 bottles = int(input("Enter number of bottles: "))
 
+#Determine if a positive number was used, end early if 0 or less than zero
 if bottles <= 0:
+
+#use input() to prevent quit() until input is recieved
     input("Time to buy more bottles of beer")
     quit()
 else:
+#Begin a loop to recite the song for each bottle 
     while bottles != 0 :
         print(f"{bottles} bottles of beer on the wall, {bottles} bottles of beer.")
         print("")
+
+        #decrement by one before continuing the loop and song
         bottles = bottles - 1
         print(f"Take one down, pass it around, {bottles} bottle(s) of beer on the wall.")
-        
+
+#use input() to prevent quit() until input is recieved
 input("Time to buy more bottles of beer.")
 quit()
         
